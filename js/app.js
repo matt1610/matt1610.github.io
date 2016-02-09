2c4de06a9e6afedc9dda08475e6fb5e67ad48f44
Matthew Starkey <mattstarkey@me.com>
2016-02-09 10:48:32 +0200

    Cap

--- js/app.js
+++ js/app.js
@@ -6,30 +6,15 @@ var App = /**
 	angular.module('App', [])
 
 	.controller('MainCtrl', ['$scope', function($scope){
-		$scope.Projects = [
-			{
-				Name : 'Raptor Camera Project',
-				Description : 'Firmware for Raptor cameras',
-				Url : 'https://github.com/matt1610/RaptorFirmware',
-				Image : 'Image url'
-			},
-			{
-				Name : 'My Project',
-				Description : 'Project Description',
-				Url : 'Link to github page',
-				Image : 'Image url'
-			},
-			{
-				Name : 'My Project',
-				Description : 'Project Description',
-				Url : 'Link to github page',
-				Image : 'Image url'
-			},
-			{
-				Name : 'My Project',
-				Description : 'Project Description',
-				Url : 'Link to github page',
-				Image : 'Image url'
-			},
-		]
+
+		$scope.FirstCap = false;
+		
+		$scope.CheckWord = function (argument) {
+			if ($scope.InputWord[0] === $scope.InputWord[0].toUpperCase()) {
+				$scope.FirstCap = true;
+			} else {
+				$scope.FirstCap = false;
+			}
+		}
+
 	}])
